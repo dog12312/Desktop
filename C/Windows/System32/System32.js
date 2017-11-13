52f6b930f7aa66dc2e89e1bda5268d600957cd56
dog12312 <dog12312@users.noreply.github.com>
2017-11-13 11:46:53 -0500

    Create System32.js

---
+++ C/Windows/System32/System32.js
@@ -0,0 +1,26 @@
+function bootup() {
+    document.getElementById("bootupframe").style.display = "block";
+}
+function bootupdone() {
+    document.getElementById("bootupframe").style.display = "none";
+}
+function sleep() {
+    document.getElementById("sleepframe").style.display = "block";
+}
+
+function wake() {
+    document.getElementById("sleepframe").style.display = "none";
+    setTimeout(sleep, 600000);
+}
+window.onmousemove = wake;
+window.onmousedown = wake;
+window.onclick = wake;
+window.onscroll = wake;
+window.onkeypress = wake;
+function OpenWBM() {
+    document.getElementById("WBMoverlay").style.display = "block";
+}
+
+function CloseWBM() {
+    document.getElementById("WBMoverlay").style.display = "none";
+}
